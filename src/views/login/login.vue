ad1fced05976d5ed5b3265557c9ea3971434b648
zhangyumo <479096605@qq.com>
2023-02-10 00:09:57 +0800

    调试本地node服务

---
+++ src/views/login/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+       <div @click="handleClick">
+        点我 点我 点我
+       </div> 
+    </div>
+</template>
+
+<script setup lang="ts">
+import {} from "vue"
+import axios from "axios"
+
+function handleClick(){
+    axios.post('http://localhost:3000/api/user/register').then(res =>{
+        console.log(res,'res');
+        
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
